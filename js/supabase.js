70724fe2a2da1f96a5bc89ce6504f6241a87d419
naderh12 <hbtc.29729@gmail.com>
2026-09-20 14:49:33 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,24 @@
+const SUPABASE_URL = "https://paimizculszksstmcfsq.supabase.co";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhaW1pemN1bHN6a3NzdG1jZnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4MDk4NTYsImV4cCI6MjEwNTM4NTg1Nn0.9MlGrXKL745jPWygSuOSWVh-eLX-sR565zHYDRuufK0";
+
+async function addAttendance(sessionId, studentId){
+  const response = await fetch(
+    `${SUPABASE_URL}/rest/v1/attendance_temp`,
+    {
+      method:'POST',
+      headers:{
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${SUPABASE_KEY}`,
+        'Content-Type':'application/json',
+        Prefer:'return=representation'
+      },
+      body: JSON.stringify({
+        session_id: sessionId,
+        student_id: studentId
+      })
+    }
+  );
+
+  return response;
+}
